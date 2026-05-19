1cb42dd099992cef3605c55929adc8814dd00ce8
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:33:03 -0400

    Add files via upload

---
+++ MicroProducto/src/main/java/com/example/MicroProducto/MicroProductoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroProducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroProductoApplication.class, args);
+	}
+
+}
---
+++ MicroProducto/src/test/java/com/example/MicroProducto/MicroProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroProducto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
