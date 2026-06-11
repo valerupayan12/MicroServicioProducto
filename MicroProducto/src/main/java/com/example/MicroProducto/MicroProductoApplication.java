b2697efbc436e6002615ad70a4c42cd3b2272834
dieriverar28 <die.riverar@duocuc.cl>
2026-06-11 13:45:29 -0400

    producto con eureka intregado

--- MicroProducto/src/main/java/com/example/MicroProducto/MicroProductoApplication.java
+++ MicroProducto/src/main/java/com/example/MicroProducto/MicroProductoApplication.java
@@ -2,8 +2,11 @@ package com.example.MicroProducto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroProductoApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +14,4 @@ public class MicroProductoApplication {
 	}
 
 }
+
